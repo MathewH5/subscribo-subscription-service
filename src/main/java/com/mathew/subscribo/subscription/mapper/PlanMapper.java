67a86bbe5edf4a73058e8b8f9aaf5d35b688c84b
Mathew <matheoshanna10@gmail.com>
2026-02-03 21:39:37 -0300

    feat: mapper

--- src/main/java/com/mathew/subscribo/subscription/mapper/PlanMapper.java
+++ src/main/java/com/mathew/subscribo/subscription/mapper/PlanMapper.java
@@ -1,2 +1,24 @@
-package com.mathew.subscribo.subscription.mapper;public class PlanMapper {
+package com.mathew.subscribo.subscription.mapper;
+
+import com.mathew.subscribo.subscription.model.PlanResponse;
+import com.mathew.subscribo.subscription.model.enitty.PlanEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlanMapper {
+
+    public PlanResponse toResponse (PlanEntity entity){
+        if (entity == null) {
+            return null;
+        }
+        return new PlanResponse(
+                entity.getId(),
+                entity.getName(),
+                entity.getDescription(),
+                entity.getPrice(),
+                entity.getBillingCycle(),
+                entity.getStatus(),
+                entity.getCreatedAt()
+        );
+    }
 }
--- src/main/java/com/mathew/subscribo/subscription/mapper/SubscriptionMapper.java
+++ src/main/java/com/mathew/subscribo/subscription/mapper/SubscriptionMapper.java
@@ -1,2 +1,65 @@
-package com.mathew.subscribo.subscription.mapper;public class SubscriptionMapper {
+package com.mathew.subscribo.subscription.mapper;
+
+import com.mathew.subscribo.subscription.model.BillingCycle;
+import com.mathew.subscribo.subscription.model.SubscriptionRequest;
+import com.mathew.subscribo.subscription.model.SubscriptionResponse;
+import com.mathew.subscribo.subscription.model.enitty.SubscriptionEntity;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class SubscriptionMapper {
+
+    public SubscriptionEntity toEntity(SubscriptionRequest request){
+        if (request == null) {
+            return null;
+        }
+
+        SubscriptionEntity entity = new SubscriptionEntity();
+
+        entity.setCustomerId(request.customerId());
+        entity.setPlanId(request.planId());
+        entity.setStatus(request.status());
+        entity.setCurrentPrice(request.currentPrice());
+        entity.setBillingCycle(request.billingCycle());
+
+        LocalDateTime now = LocalDateTime.now();
+        entity.setStartDate(now);
+
+        if (request.billingCycle() == BillingCycle.MONTHLY) {
+            entity.setEndDate(now.plusMonths(1)); // Adiciona 1 mês exato (ex: 31/jan -> 28/fev)
+        } else if (request.billingCycle() == BillingCycle.YEARLY) {
+            entity.setEndDate(now.plusYears(1));
+        }
+        entity.setNextBillingDate(entity.getEndDate());
+
+        entity.setInitialPrice(request.currentPrice());
+
+        return entity;
+    }
+
+    public SubscriptionResponse toResponse(SubscriptionEntity entity){
+        if (entity == null) {
+            return null;
+        }
+        return new SubscriptionResponse(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getPlanId(),
+                entity.getStatus(),
+                entity.getInitialPrice(),
+                entity.getCurrentPrice(),
+                entity.getBillingCycle(),
+                entity.getScheduledPlanId(),
+                entity.getScheduledBillingCycle(),
+                entity.getScheduledPrice(),
+                entity.getTrialEndsAt(),
+                entity.getStartDate(),
+                entity.getEndDate(),
+                entity.getNextBillingDate(),
+                entity.getCanceledAt(),
+                entity.getCreatedAt()
+        );
+    }
 }
