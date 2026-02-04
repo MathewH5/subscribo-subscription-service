edd4e753ef369ccad73b5abcd1239c62099a53e3
Mathew <matheoshanna10@gmail.com>
2026-02-04 20:07:43 -0300

    faet: controller

--- src/main/java/com/mathew/subscribo/subscription/controller/PlanController.java
+++ src/main/java/com/mathew/subscribo/subscription/controller/PlanController.java
@@ -1,2 +1,37 @@
-package com.mathew.subscribo.subscription.controller;public class PlanController {
+package com.mathew.subscribo.subscription.controller;
+
+import com.mathew.subscribo.subscription.model.PlanResponse;
+import com.mathew.subscribo.subscription.service.GetPlanService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/plan")
+public class PlanController {
+
+    private final GetPlanService getPlanService;
+
+    public PlanController(GetPlanService getPlanService) {
+        this.getPlanService = getPlanService;
+    }
+
+    @GetMapping
+    public List<PlanResponse> getPlans(){
+        return getPlanService.getPlans();
+    }
+
+    @GetMapping({"/active"})
+    public List<PlanResponse> getPlanActive(){
+        return getPlanService.listActivePlans();
+    }
+
+    @GetMapping("/{id}")
+    public PlanResponse getPlanId(@PathVariable Long id){
+        return getPlanService.getPlanById(id);
+    }
+
 }
--- src/main/java/com/mathew/subscribo/subscription/controller/SubscriptionController.java
+++ src/main/java/com/mathew/subscribo/subscription/controller/SubscriptionController.java
@@ -1,2 +1,54 @@
-package com.mathew.subscribo.subscription.controller;public class SubscriptionController {
+package com.mathew.subscribo.subscription.controller;
+
+import com.mathew.subscribo.subscription.model.ChangeSubscriptionPlanRequest;
+import com.mathew.subscribo.subscription.model.CustomerResponse;
+import com.mathew.subscribo.subscription.model.SubscriptionRequest;
+import com.mathew.subscribo.subscription.model.SubscriptionResponse;
+import com.mathew.subscribo.subscription.service.ChangeSubscriptionPlanService;
+import com.mathew.subscribo.subscription.service.CreateSubscriptionService;
+import com.mathew.subscribo.subscription.service.GetCustomerFromSubscriptionService;
+import com.mathew.subscribo.subscription.service.GetSubscriptionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subscription")
+public class SubscriptionController {
+
+    private final GetSubscriptionService getSubscription;
+    private final CreateSubscriptionService createSubscription;
+    private final GetCustomerFromSubscriptionService getCustomerFromSubscription;
+    private final ChangeSubscriptionPlanService changeSubscriptionPlan;
+
+    public SubscriptionController(GetSubscriptionService getSubscriptionService, CreateSubscriptionService createSubscription, GetCustomerFromSubscriptionService getCustomerFromSubscriptionService, ChangeSubscriptionPlanService changeSubscriptionPlanService) {
+        this.getSubscription = getSubscriptionService;
+        this.createSubscription = createSubscription;
+        this.getCustomerFromSubscription = getCustomerFromSubscriptionService;
+        this.changeSubscriptionPlan = changeSubscriptionPlanService;
+    }
+
+    @GetMapping
+    public List<SubscriptionResponse> getSubscription(){
+        return getSubscription.execute();
+    }
+
+    @GetMapping("/{customerid}")
+    public CustomerResponse getCustomerFromSubscription(@PathVariable Long customerid){
+        return getCustomerFromSubscription.execute(customerid);
+    }
+
+    @PostMapping
+    public ResponseEntity<SubscriptionResponse> createSubscription(@RequestBody SubscriptionRequest request){
+        SubscriptionResponse created = createSubscription.execute(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{subscriptionId}/plan")
+    public ResponseEntity<SubscriptionResponse> updateSubscriptionPlan(@PathVariable Long subscriptionId, @RequestBody ChangeSubscriptionPlanRequest request){
+        SubscriptionResponse update = changeSubscriptionPlan.execute(subscriptionId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(update);
+    }
 }
