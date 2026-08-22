118f3695a7840e3f8115c64edd16303e8d8743b8
Mathew <matheoshanna10@gmail.com>
2026-08-22 14:58:45 -0300

    feat: externalizar url

---
+++ src/main/java/com/mathew/subscribo/subscription/config/CustomerClientProperties.java
@@ -0,0 +1,14 @@
+package com.mathew.subscribo.subscription.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.net.URI;
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "clients.customer")
+public record CustomerClientProperties(
+        URI baseUrl,
+        Duration connectTimeout,
+        Duration responseTimeout
+) {
+}
